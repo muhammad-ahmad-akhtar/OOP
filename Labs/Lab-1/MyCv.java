82799ecb1b7decdd8022be91af5367e69db7cc47
Ahmad Akhtar <360akhtar@gmail.com>
2025-02-16 16:10:28 +0500

    Done Lab-1 tasks.

---
+++ Labs/Lab-1/MyCv.java
@@ -0,0 +1,13 @@
+class MyCv{
+
+	public static void main(String[] args){
+		System.out.printf("------------------------------------------------------------------------------");
+		System.out.printf("\n%20s\t%15s\t\t%10s\t%5s\n", "Institute", "Degree", "Obt. Marks", "Total Marks");
+		System.out.printf("------------------------------------------------------------------------------");
+		System.out.printf("\n %01d %20s\t%15s\t\t%10d\t%5d\n", 1,"The Trust School", "Matriculation", 1076, 1100);
+		System.out.printf("\n %01d %20s\t%15s\t\t%10d\t%5d\n", 2,"Punjab College", "Intermediate", 913, 1100);
+		System.out.printf("\n %01d %20s\t%15s\t\t%10.1f\t%5.1f\n", 3,"Comsats, Lahore", "BS. CS", 0.0, 4.0);
+		System.out.printf("------------------------------------------------------------------------------");
+	}
+
+}
